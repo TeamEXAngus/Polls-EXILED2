89af0e96a0369d167a299b782df3f3f021ba4d99
Will Morris <teamexangus@gmail.com>
2021-07-04 15:05:57 +1000

    Version counter smh

--- Polls/PollsPlugin.cs
+++ Polls/PollsPlugin.cs
@@ -10,7 +10,7 @@ namespace Polls
         public static PollsPlugin Instance;
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
-        public override Version Version { get; } = new Version(1, 0, 6);
+        public override Version Version { get; } = new Version(1, 0, 7);
 
         public Poll ActivePoll = null;
 
